c91c2e4978b18086ad138fb3fe3a80af17e1264e
Kuba <236004@edu.p.lodz.pl>
2023-03-28 17:38:05 +0200

    dodanie kodu i pustych plikow do testowania

---
+++ lab4/main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import sys
+
+def my_printf(format_string,param):
+    #print(format_string)
+    shouldDo=True
+    result=""
+    result2=""
+    for idx in range(0,len(format_string)):
+        if shouldDo:
+            if format_string[idx] == '#' and format_string[idx+1] == 'g':
+                shouldDo=False
+                if param == "":
+                   continue
+                result2+=str(int(eval(param)))
+                result+= str(result2[::-1])
+                result=str(int(result))
+                print(result,end="")
+            else:
+                print(format_string[idx],end="")
+        else:
+            shouldDo=True
+    print("")
+
+data=sys.stdin.readlines()
+
+for i in range(0,len(data),2):
+    my_printf(data[i].rstrip(),data[i+1].rstrip())
